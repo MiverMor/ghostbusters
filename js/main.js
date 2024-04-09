9c905388f3c1c6eca7209642371e3eb6aab10171
MiverMor <ivan.mordyashev@mail.ru>
2024-04-09 18:32:39 +0800

    create: clider news block

--- js/main.js
+++ js/main.js
@@ -5,4 +5,28 @@ $(document).ready(function () {
     mMenu.toggleClass("active");
     $('body').toggleClass("no-scroll");
   });
+
+  const Myswiper = new Swiper('.swiper', {
+  // Optional parameters
+  slidesPerView: 4,
+  spaceBetween: 25,
+  loop: true,
+  
+  breakpoints: {
+    992: {
+      slidesPerView: 4,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    320: {
+      slidesPerView: 1,
+      navigation: {
+        nextEl: ".news__button--next",
+      },
+    },
+  }
+
+
+});
 });
